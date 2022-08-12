e1ca9cf74701d4960591fcc0a37e298218d3121d
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-08-12 16:41:23 -0300

    Avançando na teoria do laravel

---
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,35 @@
+<h3>Fornecedor</h3>
+
+@php
+    /* 
+    if(empty(variavel)) {} //retorna true se a variável estiver vazia
+    
+    Situações que o empty retornará True
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var 
+    */
+@endphp
+
+
+@isset($fornecedores)
+
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i])):
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{$fornecedores[$i]['ddd'] ?? ''}}) ({{ $fornecedores[$i]['telefone'] ?? '' }})
+        <hr>
+        @php $i++ @endphp
+    @endwhile
+@endisset
+
