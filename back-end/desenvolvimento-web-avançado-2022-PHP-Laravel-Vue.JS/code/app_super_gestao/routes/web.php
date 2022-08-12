c3ea88fb39afe4c10caef3109f439415ebed59b0
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-08-12 13:52:28 -0300

    Avançando no curso de laravel - módulo 6 concluído (Avançando nas Rotas)

---
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PrincipalController@principal') -> name('site.index');
+Route::get('/sobre-nós', 'SobreNosController@sobreNos') -> name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato') -> name('site.contato');
+Route::get('/login', 'LoginController@login') -> name('site.login');
+
+
+Route::prefix('/app') -> group(function() {
+    Route::get('/clientes', 'ClientesController@clientes') -> name('app.clientes');
+    Route::get('/fornecedores', 'FornecedoresController@fornecedores') -> name('app.fornecedores');
+    Route::get('/produtos', 'ProdutosController@produtos') -> name('app.produtos');
+});
+
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+}) -> name('site.rota1');
+
+
+Route::get('/rota2', function() {
+    return redirect() -> route('site.rota1');
+}) -> name('site.rota2');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para página inicial.';
+});
+
+//Route::redirect('/rota2', '/rota1');
+
+
+
+// nome, categoria, assunto, mensagem
+// Route::get(
+//     '/contato/{nome}/{categoria_id}', // 
+//     function(
+//         string $nome = 'Desconhecido',
+//         int $categoria_id  = 1 // 1 - 'Informação'
+//     ) {
+//          echo "Estamos aqui: $nome - $categoria_id";
+//     }
+// ) -> where('categoria_id', '[0-9]+') 
+//   -> where('nome', '[A-Za-z]+');
+
+// Route::get($uri, $callback);
+
+// Route::get('/', function () {
+//     return 'Olá, seja bem vindo ao curso!';
+// });
