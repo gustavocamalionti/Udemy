f4a5989007a33956517193c0e0a370b0393bba39
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-08-18 17:29:17 -0300

    validação de campos obrigatórios e Repopulando um formulário após atualizar a página

--- back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,11 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        var_dump($_POST);
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+    public function contato(Request $request) {
+        //var_dump($_POST);
+        //dd($request);
+
+        // echo '<pre>';
+        //     print_r($request->all());
+
+        // echo '</pre>';
+
+        // echo $request->input('nome');
+        // echo '<br>';
+        
+        // echo $request->input('mensagem');
+        // echo '<br>';
+
+        //$contato = new SiteContato();
+        //$contato-> nome = $request->input('nome');
+        //$contato-> telefone  = $request->input('telefone');
+        //$contato-> email  = $request->input('email');
+        //$contato-> motivo_contato  = $request->input('motivo_contato');
+        //$contato-> mensagem  = $request->input('mensagem');
+
+        //$contato = new SiteContato();
+        //$contato->fill($request->all());
+
+        //print_r($contato->getAttributes());
+        //$contato->save();
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+        //Realizar a validação dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40', //Nomes com no mínimo 3 caracteres e no máximo 40
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+
+        SiteContato::create($request->all());
     }
 };
--- back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/resources/views/site/principal.blade.php
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_super_gestao/resources/views/site/principal.blade.php
@@ -29,8 +29,6 @@
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
                 @component('site.layouts._components.form_contato', ['classe' => 'borda-branca'])
-                    <p>A nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possível</p>
-                    <p>Nosso tempo médio de resposta é de 48 horas.</p>
                 @endcomponent
             </div>
         </div>
