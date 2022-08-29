019706bd0bcc8bd39065acdb9b6c1dc5d737b6d9
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-08-29 08:33:56 -0300

    avançando no curso

--- back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_controle_tarefas/routes/web.php
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_controle_tarefas/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTesteMail;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('verified');
+
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
+    ->middleware('verified');
+
+Route::get('/mensagem-teste', function() {
+    return new MensagemTesteMail();
+    //Mail::to('gustavocamalionti@gmail.com')->send(new MensagemTesteMail());
+    //return 'Email enviado com Sucesso';
+});
