9c40bfd93835b356bb925af23545bed25dc00794
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-08-31 10:16:04 -0300

    Laravel make:mail | Avançando no envio de email automático

--- back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+use App\Mail\NovaTarefaMail;
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Auth;
@@ -59,7 +61,14 @@ class TarefaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $dados = $request->all('tarefa', 'data_limite_conclusao');
+        $dados['user_id'] = auth()->user()->id;
+        
+        $tarefa = Tarefa::create($dados);
+        $destinatario = auth()->user()->email; //e-mail do usuário logado (autenticado)
+        Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
+
+        return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
 
     /**
@@ -70,6 +79,7 @@ class TarefaController extends Controller
      */
     public function show(Tarefa $tarefa)
     {
+        return view('tarefa.show', ['tarefa' => $tarefa]);
         //
     }
 
