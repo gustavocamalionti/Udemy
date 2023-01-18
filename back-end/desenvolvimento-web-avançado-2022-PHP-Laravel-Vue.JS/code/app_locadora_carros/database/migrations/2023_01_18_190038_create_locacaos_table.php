ff7682bd8ee0fd918cd0beac0b58fa1335489c4a
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-01-18 16:18:43 -0300

    Ajustando migrations com as tabelas

--- back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_locadora_carros/database/migrations/2023_01_18_190038_create_locacaos_table.php
+++ back-end/desenvolvimento-web-avançado-2022-PHP-Laravel-Vue.JS/code/app_locadora_carros/database/migrations/2023_01_18_190038_create_locacaos_table.php
@@ -13,9 +13,21 @@ class CreateLocacaosTable extends Migration
      */
     public function up()
     {
-        Schema::create('locacaos', function (Blueprint $table) {
+        Schema::create('locacoes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('carro_id');
+            $table->dateTime('data_inicio_periodo');
+            $table->dateTime('data_final_previsto_periodo');
+            $table->dateTime('data_final_realizado_periodo');
+            $table->float('valor_diaria', 8,2);
+            $table->integer('km_inicial');
+            $table->integer('km_final');
             $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->foreign('carro_id')->references('id')->on('carros');
         });
     }
 
