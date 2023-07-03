5a1a3c912edd246dc94788b1bb628b32e5437a14
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2023-07-03 09:39:53 -0300

    :sparkles: finish classroom console-log

---
+++ full-stack/javascript-typescript-basico-avancado/secao-2-javascript-basico/7-console-log/exercises.js
@@ -0,0 +1,5 @@
+var strName = 'Gustavo Camalionti Gomes';
+var strLinguage = 'JavaScript';
+var strHours = '9:33';
+
+console.log('Olá, meu nome é', strName, '. Estou aprendendo', strLinguage, 'ás', strHours, 'da manhã.' );
---
+++ full-stack/javascript-typescript-basico-avancado/secao-2-javascript-basico/7-console-log/script.js
@@ -0,0 +1,8 @@
+// ';' é opcional, porém é interessante sempre mante-lo para entender melhor os inicios e finais.
+
+console.log('Gustavo Camalionti'); // String = Texto
+console.log("Gustavo 'Camalionti'"); //Colocar um texto entre aspas simples dentro da strnig
+console.log(`'Gustavo' "Camalionti"`); //template strings, pode utilizar aspas simples e aspas duplas
+
+console.log(123456, 15633.35019); //não quebra linha Number = Número
+console.log(35, 15.85, 'Gustavo Camalionti');
